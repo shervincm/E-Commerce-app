b9949da389c20e0e62e270fda52283d41a3028d9
Shervin <shervinmohajeri@gmail.com>
2023-07-11 15:40:51 +0100

    created an image carousel

--- my-app/src/Components/ItemOverview/itemOverview.js
+++ my-app/src/Components/ItemOverview/itemOverview.js
@@ -1,15 +1,34 @@
 import {useParams} from 'react-router-dom';
+import { useState } from 'react';
 
 function ItemOverview({items}) {
     const {id} = useParams();
 
     const selectedItem = items.find(item => item._id === id);
+    console.log(selectedItem);
+    const itemImage = selectedItem.imageURL;
+    console.log(itemImage);
+    
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
   
-    // const selectedItemId = selectedItem ? selectedItem._id : null;
+  function handleClick() {
+    if (currentImageIndex < itemImage.length - 1) {
+      setCurrentImageIndex(currentImageIndex + 1);
+    }
+  }
+
+  function backClick() {
+    if (currentImageIndex > 0) {
+      setCurrentImageIndex(currentImageIndex - 1);
+    }
+    console.log(currentImageIndex);
+  }
 
 return (    
     <div>
-        <img src={selectedItem.imageURL[0].url} alt={selectedItem.title} />
+        <button onClick={backClick}> previous image </button>
+        <img src={itemImage[currentImageIndex].url} alt={selectedItem.title}/>
+        <button onClick={handleClick}> next image </button> 
     </div>
 )
 
